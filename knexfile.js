417c69ad3dff0ff5a5a3d0707c1877adc61b4e67
Konstantin Mack <mack.konstantin@googlemail.com>
2022-08-16 11:21:51 +0100

    fix mysql client typo

--- knexfile.js
+++ knexfile.js
@@ -16,7 +16,7 @@ const connections = {
     },
   },
   production: {
-    client: "mysql",
+    client: "mysql2",
     connection: {
       host: process.env.DB_HOST,
       user: process.env.DB_USERNAME,
